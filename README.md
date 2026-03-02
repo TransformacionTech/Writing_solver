f098de6ed0a0846883fd8675e9aa5f281e12fe74
ciro mc <156836104+Ciricundio@users.noreply.github.com>
2026-03-02 13:30:40 -0500

    New configuration

--- README.md
+++
@@ -1,6 +0,0 @@
----
-title: writing_solver
-app_file: crew.py
-sdk: gradio
-sdk_version: 6.6.0
----
--- tasks/researcherTask.py
+++
@@ -1,9 +0,0 @@
-from crewai import Task
-from agents import researcherAgent
-
-#Definir tareas
-researchTask = Task(
-    description = 'Investigar sobre {topic} y encontrar 10 puntos claves',
-    agent = researcherAgent.researcher,
-    expected_output = 'Lista de 10 puntos relevantes sobre {topic}'
-)
